8dd0695a606ad4f01e48ce2111996bb09c3ed4a7
fawen.he <fawen.he@riskeys.com>
2020-12-12 00:07:46 +0800

    修改r2dbc mapper自动扫描

--- src/main/java/com/wendel/config/R2dbcAutoConfiguration.java
+++ src/main/java/com/wendel/config/R2dbcAutoConfiguration.java
@@ -1,16 +1,22 @@
 package com.wendel.config;
 
 import io.r2dbc.spi.ConnectionFactory;
-import org.apache.ibatis.builder.xml.XMLConfigBuilder;
+import org.apache.ibatis.builder.xml.XMLMapperBuilder;
 import org.apache.ibatis.r2dbc.ReactiveSqlSession;
 import org.apache.ibatis.r2dbc.ReactiveSqlSessionFactory;
 import org.apache.ibatis.r2dbc.impl.DefaultReactiveSqlSessionFactory;
+import org.apache.ibatis.r2dbc.type.EnumOrdinalTypeHandler;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.data.r2dbc.connectionfactory.R2dbcTransactionManager;
 import org.springframework.transaction.ReactiveTransactionManager;
+
+import java.io.IOException;
+import java.math.RoundingMode;
 
 /**
  * @author ：sunrise
@@ -20,11 +26,24 @@ import org.springframework.transaction.ReactiveTransactionManager;
  */
 @Configuration
 public class R2dbcAutoConfiguration{
-
     @Bean
     public ReactiveSqlSessionFactory reactiveSqlSessionFactory(ConnectionFactory connectionFactory)  {
-        XMLConfigBuilder xmlConfigBuilder = new XMLConfigBuilder(this.getClass().getResourceAsStream("/config/mybatis-config.xml"));
-        org.apache.ibatis.session.Configuration configuration = xmlConfigBuilder.parse();
+//        XMLConfigBuilder xmlConfigBuilder = new XMLConfigBuilder(this.getClass().getResourceAsStream("/config/mybatis-config.xml"));
+//        org.apache.ibatis.session.Configuration configuration = xmlConfigBuilder.parse();
+
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        try {
+            configuration.getTypeHandlerRegistry().register(RoundingMode.class, EnumOrdinalTypeHandler.class);
+
+            Resource[] resources = new PathMatchingResourcePatternResolver().getResources("classpath*:mybatis/**/*.xml");
+            for (Resource resource : resources) {
+                XMLMapperBuilder mapperParser = new XMLMapperBuilder(resource.getInputStream(), configuration, resource.getFilename(), configuration.getSqlFragments());
+                mapperParser.parse();
+                configuration.addLoadedResource(resource.getFilename());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new DefaultReactiveSqlSessionFactory(configuration, connectionFactory);
     }
 
